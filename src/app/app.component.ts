8f2b930815f8a73ec11ddea8970919f5dd32c653
Deepthi Varsha <dperiyan@cisco.com>
2021-02-15 20:04:54 +0530

    session idle timeout handled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { BnNgIdleService } from 'bn-ng-idle';
+import { Router } from '@angular/router';
+import { WebexService } from './services/webex.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +11,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-webex';
-  constructor(){
+  // session idle check
+  constructor(private bnIdle: BnNgIdleService, private route: Router, private webex: WebexService) {
+    this.bnIdle.startWatching(environment.TIMEOUT).subscribe((res) => {
+      window.alert('Session timed out after being kept idle for long. Please login again');
+      this.webex.onLogout();
+    });
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
   client_id: 'C5ec2fbf8a225ef46e5fadc3f17c944b761468efcf30f965f7c2b5ca6b5495b0b',
   client_secret: 'cbd8fa39a42a90992bf8024dadf6e5176a6463132fb0e4ac02581194462266ca',
   redirect_uri: 'http://localhost:4200/roomcontrol',
-  scope: 'spark:all spark:kms'
+  scope: 'spark:all spark:kms',
+  TIMEOUT: 3600
 };
 
 /*
