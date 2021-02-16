c99c996196645aaad34ceafaabc758627923be4d
Deepthi Varsha <dperiyan@cisco.com>
2021-02-16 12:32:16 +0530

    update environment prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  client_id: 'C5ec2fbf8a225ef46e5fadc3f17c944b761468efcf30f965f7c2b5ca6b5495b0b',
+  client_secret: 'cbd8fa39a42a90992bf8024dadf6e5176a6463132fb0e4ac02581194462266ca',
+  redirect_uri: 'https://deepthivarsha.github.io/manage-webex-rooms/roomcontrol',
+  scope: 'spark:all spark:kms',
+  TIMEOUT: 3600
 };
